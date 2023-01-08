486084d2ed0eef88571e5ca1890f342e9d1770da
eqan <eqan.ahmad123@gmail.com>
2023-01-08 07:12:27 +0500

    Fixed User Filter Problem

--- src/users/dto/filter.users.dto.ts
+++ src/users/dto/filter.users.dto.ts
@@ -1,10 +1,8 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEthereumAddress } from 'class-validator';
 import { PaginationParam } from './pagination.dto';
 
 @InputType()
 export class FilterUserDto extends PaginationParam {
-  @IsEthereumAddress()
   @Field({ nullable: true, defaultValue: undefined })
   id?: string;
 }
