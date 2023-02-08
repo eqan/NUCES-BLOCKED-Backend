450c695f7133c326069e93f3d1a8054fe3311992
eqan <eqan.ahmad123@gmail.com>
2023-02-08 10:50:24 +0500

    3-cron-job-implementation-for-fetching-data-from-blockchain: Small Modifications but still testing needs to be done

--- src/semester-results/semester-result.module.ts
+++ src/semester-results/semester-result.module.ts
@@ -1,8 +1,11 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { ScheduleModule, SchedulerRegistry } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisModule, RedisService } from 'nestjs-redis';
 import { AuthModule } from 'src/auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
 import { SemesterResult } from './entities/semester-result.entity';
+import { SemesterCron } from './semester-result-cron.service';
 import { SemesterResultResolver } from './semester-result.resolver';
 import { SemesterResultService } from './semester-result.service';
 
@@ -11,7 +14,14 @@ import { SemesterResultService } from './semester-result.service';
     TypeOrmModule.forFeature([SemesterResult]),
     forwardRef(() => AuthModule),
   ],
-  providers: [SemesterResultResolver, SemesterResultService, AuthService],
+  providers: [
+    SemesterResultResolver,
+    SchedulerRegistry,
+    RedisService,
+    SemesterCron,
+    SemesterResultService,
+    AuthService,
+  ],
   exports: [SemesterResultService],
 })
 export class SemesterResultModule {}
