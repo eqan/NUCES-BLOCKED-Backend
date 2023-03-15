4a9a6b7140da1d227e95c2363f7a9a4b706c3450
eqan <eqan.ahmad123@gmail.com>
2023-03-15 05:27:13 +0500

    16-nft-storage-integeration-modifications: Eliminated redis

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 # Script for starting the server and running the project with 1 command
 sudo service postgresql start
-sudo service redis-server start
+# sudo service redis-server start
 yarn start:debug
